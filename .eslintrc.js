a8e1a974e5b96fe47126bc28a0c3ec0317018d42
oscar <oscarmancilla227@gmail.com>
2025-01-06 18:47:32 -0600

    tarea agregado

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,38 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // Reglas más flexibles
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn', // Cambiado a warning en lugar de error
+    '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'prettier/prettier': ['error', {
+      'endOfLine': 'auto'
+    }],
   },
+  // Ignora ciertos archivos y directorios
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '*.js',
+    '*.jsx',
+  ]
 };
